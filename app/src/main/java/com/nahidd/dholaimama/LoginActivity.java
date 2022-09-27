2b72cf8e4223fda0886b8e9105e4469ba2eef95e
Nahid009 <nahid_hasan009@yahoo.com>
2022-09-27 15:19:19 +0600

    DholaiMama

---
+++ app/src/main/java/com/nahidd/dholaimama/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.nahidd.dholaimama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/nahidd/dholaimama/SplashScreen.java
+++ app/src/main/java/com/nahidd/dholaimama/SplashScreen.java
@@ -3,12 +3,21 @@ package com.nahidd.dholaimama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    CircleImageView circleImageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        circleImageView = findViewById(R.id.splashImage);
+        textView = findViewById(R.id.splashScreenTextView);
     }
 }
